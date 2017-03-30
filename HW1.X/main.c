796a14bc1c3bcc4a348f3b7f4c93dcb36c633a52
Elutzen <Elutzen@gmail.com>
2017-03-29 21:54:23 -0500

    Hello World Code

---
+++ HW1.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: elutz
+ *
+ * Created on March 29, 2017, 9:42 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
